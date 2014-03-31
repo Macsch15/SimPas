e437f5c90a1fe0c4ba3c7fa774e22f77da4b0024
Maciej <macsch15@gmail.com>
2014-03-31 18:44:37 +0200

    Don't need all fields

--- library/Application/Pastebin/ReadPaste.php
+++ library/Application/Pastebin/ReadPaste.php
@@ -37,7 +37,8 @@ class ReadPaste
         // Prepare query
         $query = $this->data_source
         ->get()
-        ->prepare('SELECT * FROM ' . $this->config('Database')->prefix  . 'pastes WHERE unique_id = :paste_id LIMIT 1');
+        ->prepare('SELECT unique_id, time, size, length, syntax, content, ip_address, raw_content, title, author, start_from_line, visibility
+            FROM ' . $this->config('Database')->prefix  . 'pastes WHERE unique_id = :paste_id LIMIT 1');
 
         // Filter
         $query->bindValue(':paste_id', $paste_id, constant('PDO::PARAM_INT'));
