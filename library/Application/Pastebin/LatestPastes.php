f1ec56dbd35397eb05f7ac41a65574e546ecc9c8
Maciej <macsch15@gmail.com>
2014-03-31 18:07:03 +0200

    Public/Private paste fixed #16

--- library/Application/Pastebin/LatestPastes.php
+++ library/Application/Pastebin/LatestPastes.php
@@ -3,9 +3,12 @@ namespace Application\Pastebin;
 
 use Application\Application;
 use Application\View\View;
+use Application\Configuration\Configuration;
 
 class LatestPastes extends View
 {
+    use Configuration;
+    
     /**
     * Construct
     * 
@@ -27,6 +30,33 @@ class LatestPastes extends View
     */
     public function indexAction()
     {
+        // Template render
+        $this->render([
+            'container' => $this->publicPastesContainer()
+        ]);
+
         return $this->{'LatestPastes'};
+    }
+
+    /**
+    * Public pastes container
+    * 
+    * @return array
+    */
+    private function publicPastesContainer()
+    {
+        // Prepare query
+        $query = $this->data_source
+        ->get()
+        ->prepare('SELECT unique_id, time, syntax, title, author, visibility
+            FROM ' . $this->config('Database')->prefix  . 'pastes WHERE visibility = "public" ORDER BY time DESC LIMIT :limit');
+
+        // Filter
+        $query->bindValue(':limit', $this->config()->latest_pastes, constant('PDO::PARAM_INT'));
+
+        // Execute
+        $query->execute();
+
+        return $query->fetchAll();
     }
 }
