8c7e47f3ee30a72aede692ee360e9db092f8f320
Maciej <macsch15@gmail.com>
2014-04-07 18:10:57 +0200

    Corrected comments

--- library/Application/Security/QuestionsAndAnswers/QuestionsAndAnswers.php
+++ library/Application/Security/QuestionsAndAnswers/QuestionsAndAnswers.php
@@ -14,6 +14,7 @@ class QuestionsAndAnswers
     *
     * @param string $field_client_question
     * @param string $field_client_answer
+    * @throws ExceptionRuntime
     * @return bool
     */
     public function validate($field_client_question, $field_client_answer)
@@ -44,6 +45,7 @@ class QuestionsAndAnswers
     /**
     * Fetch random question
     * 
+    * @throws ExceptionRuntime
     * @return string
     */
     public function question()
