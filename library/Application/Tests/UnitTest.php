c261a8ce633ef7dfc13c9f75514b49d9386befb0
Maciej <macsch15@gmail.com>
2014-04-09 22:06:35 +0200

    Added some tests

--- library/Application/Tests/UnitTest.php
+++ library/Application/Tests/UnitTest.php
@@ -1,9 +1,65 @@
 <?php
-class SimPasTest extends PHPUnit_Framework_TestCase
+use Application\Autoloader;
+use Application\Application;
+use Application\HttpRequest\HttpRequest;
+use Application\Pastebin\SyntaxHighlighter;
+use Application\FileManager\FileManager;
+use Application\Pastebin\ShortenUrlApi\ShortenUrlApi;
+use Application\Security\DataFilters\PreDatabaseSave;
+
+class Tests extends PHPUnit_Framework_TestCase
 {
-    public function testExample()
+    public function __construct()
     {
-        $this->assertTrue(true);
+        // Include autoloader
+        require_once __DIR__ . '/../Autoloader.php';
+
+        // Setup autoloader
+        new Autoloader();
+    }
+
+    public function testHttpRequestPostActionDoesNotExists()
+    {
+        $this->assertFalse(HttpRequest::post('not_existing_field'));
+    }
+
+    public function testSyntaxHighligherCount()
+    {
+        $this->assertCount(222, (new SyntaxHighlighter)->languagesToArray());
+    }
+
+    public function testSyntaxHighligherParser()
+    {
+        $parse = preg_replace("/[\t\n\s]+/", '', (new SyntaxHighlighter)->parseCode('Example Text', 'Text'));
+
+        $expected = '<preclass="textpre_code"id="zclip_copy"style="font-family:monospace;"><ol><ahref="#line-1"><liclass="line_handler"id="line-1"style="font-weight:normal;vertical-align:top;font-size:14px"><divstyle="font:normalnormal1em/1.2emmonospace;margin:0;padding:0;background:none;vertical-align:top;">ExampleText</div></li></a></ol></pre>';
+        
+
+        $this->assertEquals($expected, $parse);
+    }
+
+    public function testFilemanagerNotExistingFile()
+    {
+        $this->assertFalse((new FileManager)->getContentsFromFile('SomeFile'));
+    }
+
+    public function testFilemanagerNotExistingUrl()
+    {
+        $this->assertFalse((new FileManager)->getContentsFromUrl('http://www..com'));
+    }
+
+    public function testShortenurlFail()
+    {
+        $this->assertFalse((new ShortenUrlApi)->shorten('http//www.example.com'));
+    }
+
+    public function testPredbfilterRC()
+    {
+        $this->assertEquals('example.string', (new PreDatabaseSave)->filter('#example*.string', true));
+    }
+
+    public function testPredbfilter()
+    {
+        $this->assertEquals('#example*.string', (new PreDatabaseSave)->filter('#example*.string', false));
     }
 }
-?>
