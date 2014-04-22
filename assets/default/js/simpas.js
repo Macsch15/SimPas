db69bd0e5121a772f7d22dda2af7ffbd86183719
Maciej <macsch15@gmail.com>
2014-04-22 21:58:43 +0200

    Indent standards

--- assets/default/js/simpas.js
+++ assets/default/js/simpas.js
@@ -1,58 +1,58 @@
 $(function() {
-    $('.tooltip-top').tooltip({placement: 'top', container: 'body'}) 
-    $('.tooltip-bottom').tooltip({placement: 'bottom', container: 'body'}) 
-    $('.tooltip-left').tooltip({placement: 'left', container: 'body'}) 
-    $('.tooltip-right').tooltip({placement: 'right', container: 'body'})
+  $('.tooltip-top').tooltip({placement: 'top', container: 'body'}) 
+  $('.tooltip-bottom').tooltip({placement: 'bottom', container: 'body'}) 
+  $('.tooltip-left').tooltip({placement: 'left', container: 'body'}) 
+  $('.tooltip-right').tooltip({placement: 'right', container: 'body'})
 
-    $('button#options').popover({ 
-        html : true,
-        trigger: 'manual',
-        delay: { show: 50, hide: 50 },
-        content: function() {
-          return $("#popover_content").html();
-        }
-    });
-
-    $('button#options').click(function() {
-        $('div#form_data').empty();
-
-        if($('div.popover').length == 0) {
-            $('button#options').popover('show');
-            $('div#popover_content').remove();
-        } else {
-            $('button#options').popover('hide');
-            $('div#form_data').empty();
-            $($('div.popover').clone(true, true)).appendTo('div#form_data');
-        }  
-    });
-
-    $('textarea.form-control').each(function() {
-        if ($(this).val().length == 0) {
-            $('button.submit_button').addClass('disabled');
-        }
-    });
-
-    $('textarea.form-control').bind('input propertychange', function() {
-      $('button.submit_button').addClass('disabled');
-
-      if(this.value.length) {
-        $('button.submit_button').removeClass('disabled');
-      }
-    });
-
-    if(document.location.hash) {
-        showLine();
+  $('button#options').popover({ 
+    html : true,
+    trigger: 'manual',
+    delay: { show: 50, hide: 50 },
+    content: function() {
+      return $("#popover_content").html();
     }
+  });
 
-    $('li.line_handler').click(function() {
-        $('li.line_active').removeClass('line_active');
-        $(this).addClass('line_active');
-    });
+  $('button#options').click(function() {
+    $('div#form_data').empty();
+
+    if($('div.popover').length == 0) {
+      $('button#options').popover('show');
+      $('div#popover_content').remove();
+    } else {
+      $('button#options').popover('hide');
+      $('div#form_data').empty();
+      $($('div.popover').clone(true, true)).appendTo('div#form_data');
+    }  
+  });
+
+  $('textarea.form-control').each(function() {
+    if ($(this).val().length == 0) {
+      $('button.submit_button').addClass('disabled');
+    }
+  });
+
+  $('textarea.form-control').bind('input propertychange', function() {
+    $('button.submit_button').addClass('disabled');
+
+    if(this.value.length) {
+    $('button.submit_button').removeClass('disabled');
+    }
+  });
+
+  if(document.location.hash) {
+    showLine();
+  }
+
+  $('li.line_handler').click(function() {
+    $('li.line_active').removeClass('line_active');
+    $(this).addClass('line_active');
+  });
 });
 
 function showLine() {
-    var hashName = window.location.hash;
-    var lineNumber = hashName.slice(6);
+  var hashName = window.location.hash;
+  var lineNumber = hashName.slice(6);
 
-    $('#line-' + lineNumber).addClass('line_active');
+  $('#line-' + lineNumber).addClass('line_active');
 }
