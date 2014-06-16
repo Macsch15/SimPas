5a474ca298411a0cf1fe26fbf1b1ef6c59fc586d
Maciej <macsch15@gmail.com>
2014-06-16 15:02:38 +0200

    JSON error reporting in configuration

--- library/Application/Configuration/Configuration.php
+++ library/Application/Configuration/Configuration.php
@@ -3,7 +3,7 @@ namespace Application\Configuration;
 
 use Application\FileManager\FileManager;
 use Application\Exception\AssetNotFound;
-use Application\Exception\ExceptionRuntime;
+use Application\Exception\JsonException;
 
 trait Configuration
 {
@@ -11,7 +11,10 @@ trait Configuration
      * Load Configuration File
      * 
      * @param string $entity
+     * @param bool $as_array
      * @return object|array
+     * @throws Application\Exception\JsonException
+     * @throws Application\Exception\AssetNotFound
      */
     public function config($entity = 'Default', $as_array = false)
     {
@@ -28,15 +31,44 @@ trait Configuration
             // Decode JSON as object or array
             $json_decode = json_decode((new FileManager)->getContentsFromFile($_path), ($as_array === true ?: false));
 
-            // Decode error
-            if($json_decode == null) {
-                throw new ExceptionRuntime('Something goes wrong with configuration file.');
+            switch(json_last_error()) {
+                case JSON_ERROR_NONE:
+                    $error_message = false;
+                    break;
+                case JSON_ERROR_DEPTH:
+                    $error_message = 'Maximum stack depth exceeded';
+                    break;
+                case JSON_ERROR_STATE_MISMATCH:
+                    $error_message = 'Underflow or the modes mismatch';
+                    break;
+                case JSON_ERROR_CTRL_CHAR:
+                    $error_message = 'Unexpected control character found';
+                    break;
+                case JSON_ERROR_SYNTAX:
+                    $error_message = 'Syntax error, malformed JSON';
+                    break;
+                case JSON_ERROR_UTF8:
+                    $error_message = 'Malformed UTF-8 characters, possibly incorrectly encoded';
+                    break;
+                default:
+                    $error_message = 'Unknown error';
+                    break;
             }
-            
+
+            if($error_message !== false) {
+                throw new JsonException('JSON Error:' . ' ' . $error_message . ' ' . 'in' . ' ' . $entity . '.json');
+            }
+
             return $json_decode;
-        } catch(ExceptionRuntime $exception) {
+        } catch(JsonException $exception) {
+            // Set headers
+            header('HTTP/1.1 502 Bad Gateway', true, 502);
+
             die($exception->getMessage());
         } catch(AssetNotFound $exception) {
+            // Set headers
+            header('HTTP/1.1 502 Bad Gateway', true, 502);
+
             die($exception->getMessage());
         }
     }
---
+++ library/Application/Exception/JsonException.php
@@ -0,0 +1,9 @@
+<?php
+namespace Application\Exception;
+
+use Exception;
+
+class JsonException extends Exception
+{
+
+}
