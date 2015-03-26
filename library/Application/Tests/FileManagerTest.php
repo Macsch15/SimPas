296500c80e232319bbd89eef8083bb031bf015db
Maciej Schmidt <macsch15@gmail.com>
2015-03-26 19:16:18 +0100

    Fixed build for PHP 7

--- library/Application/Tests/FileManagerTest.php
+++ library/Application/Tests/FileManagerTest.php
@@ -8,6 +8,9 @@ class FileManagerTest extends PHPUnit_Framework_TestCase
         $this->assertFalse((new FileManager)->getContentsFromFile('SomeFile'));
     }
 
+    /**
+     * @expectedException Application\Exception\ExceptionInvalidArgument
+     */
     public function testFilemanagerNotExistingUrl()
     {
         $this->assertFalse((new FileManager)->getContentsFromUrl('http://www..com'));
