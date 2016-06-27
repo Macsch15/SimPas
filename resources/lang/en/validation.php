1be592faa9c17531c7e2d03ab93845d4dfe1e2c2
Macsch15 <macsch15@gmail.com>
2016-06-27 17:09:42 +0000

    code cleanup

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -67,7 +67,7 @@ return [
     'timezone'             => 'The :attribute must be a valid zone.',
     'unique'               => 'The :attribute has already been taken.',
     'url'                  => 'The :attribute format is invalid.',
-     "recaptcha"           => 'The :attribute field is not correct.',
+    'recaptcha'            => 'The :attribute field is not correct.',
 
     'attributes' => []
 ];
