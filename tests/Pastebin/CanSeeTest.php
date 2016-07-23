ad5720d809f6503d4647af39c4c94fa065dcc611
Macsch15 <macsch15@gmail.com>
2016-07-23 00:48:39 +0000

    example test

---
+++ tests/Pastebin/CanSeeTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace SimPas\Tests;
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class CanSeeTest extends TestCase
+{
+    public function testHttpTest()
+    {
+        // ...
+    }
+}
