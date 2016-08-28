6d0225a778dfca5e41250ec07f98dbb0f9181f05
Macsch15 <macsch15@gmail.com>
2016-08-28 21:41:44 +0000

    Added tests for breadcrumb

--- tests/Breadcrumb/BreadcrumbTest.php
+++ tests/Breadcrumb/BreadcrumbTest.php
@@ -2,19 +2,81 @@
 
 namespace SimPas\Tests;
 
-use Illuminate\Foundation\Testing\WithoutMiddleware;
+use SimPas\Breadcrumb\Facades\Breadcrumb;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
 
 class BreadcrumbTest extends TestCase
 {
-    /**
-     * A basic functional test example.
-     *
-     * @return void
-     */
-    public function testTest()
+    public function testBreadcrumbIsVisible()
     {
-        $this->assertTrue(true);
+        $this->visit('/activity')
+            ->see('breadcrumb-item');
+    }
+
+    public function testBreadcrumbIsNotVisible()
+    {
+        $this->visit('/')
+            ->dontSee('breadcrumb-item');
+    }
+
+    public function testBreadcrumbShouldReceive()
+    {
+        $this->assertInstanceOf(\Illuminate\View\View::class,
+            Breadcrumb::push('Lorem ipsum'));
+    }
+
+    public function testBreadcrumbPush()
+    {
+        Breadcrumb::push('Lorem ipsum Aute eiusmod dolore.', '/page1');
+        Breadcrumb::push('Pariatur elit dolore dolor.', '/page2');
+        Breadcrumb::push('Example breadcrumb', '/page3');
+
+        $this->assertSame(Breadcrumb::count(), 3);
+    }
+
+    public function testBreadcrumbAttributes()
+    {
+        $breadcrumb = Breadcrumb::push('Hello', '/hello')
+            ->getData();
+
+        $expected = [
+            'title' => 'Hello',
+            'active' => false,
+            'url' => '/hello'
+        ];
+
+        $this->assertSame($breadcrumb, $expected);
+    }
+
+    public function testBreadcrumbActiveItem()
+    {
+        $breadcrumb = Breadcrumb::push('Hello', '/hello', true)
+            ->getData();
+
+        $expected = [
+            'title' => 'Hello',
+            'active' => true,
+            'url' => '/hello'
+        ];
+
+        $this->assertSame($breadcrumb, $expected);     
+    }
+
+    public function testBreadcrumbTemplateUrl()
+    {
+        $breadcrumb = Breadcrumb::push('Hello', '/hello')
+            ->getName();
+
+        $this->assertSame('partials.breadcrumb.url', $breadcrumb);       
+    }
+
+    public function testBreadcrumbTemplateText()
+    {
+        $breadcrumb = Breadcrumb::push('Hello')
+            ->getName();
+
+        $this->assertSame('partials.breadcrumb.text', $breadcrumb);       
     }
 }
