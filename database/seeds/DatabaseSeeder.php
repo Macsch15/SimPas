9b2e0ac2667c9f81e1ce662e90a4fdc357bb7678
Macsch15 <macsch15@gmail.com>
2016-08-28 22:09:18 +0000

    Setup postgres for travis

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        factory(\SimPas\Repository\User::class, 50)->create();
+        factory(\SimPas\Repository\PastebinRecord::class, 150)->create();
     }
 }
