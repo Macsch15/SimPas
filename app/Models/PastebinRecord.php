5f14f7ce908110d6bc0221fbc8ef4c56bdea6127
Macsch15 <macsch15@gmail.com>
2016-12-19 18:55:31 +0000

    Add model directory

---
+++ app/Models/PastebinRecord.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace SimPas\Models;
+
+use Auth;
+use Illuminate\Database\Eloquent\Model;
+
+class PastebinRecord extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'unique_id', 'user_id', 'title', 'content',
+    ];
+
+    /**
+     * Get author of pastebin.
+     *
+     * @return SimPas\Models\User
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Check if pastebin is edited.
+     *
+     * @return bool
+     */
+    public function isEdited()
+    {
+        return $this->created_at->getTimestamp() !== $this->updated_at->getTimestamp();
+    }
+
+    /**
+     * Check if current user is author of pastebin.
+     *
+     * @return bool
+     */
+    public function isAuthor()
+    {
+        return Auth::check() && Auth::id() === $this->user_id;
+    }
+
+    /**
+     * Check if pastebin is created by user.
+     *
+     * @return bool
+     */
+    public function createdByUser()
+    {
+        return $this->user_id !== 0;
+    }
+}
