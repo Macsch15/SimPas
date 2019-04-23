3df4746b439c2b9e81adf5dba20438e0fafa7a42
Macsch15 <macsch15@gmail.com>
2019-04-23 14:50:43 +0100

    Update login form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,42 +7,31 @@
         <div class="col-md-12 col-md-offset-2">
             <h4>{{ trans('auth.login') }}</h5>
 
-            {{ Form::open(['url' => route('auth.login.form')]) }}
-                <div class="form-group row{{ $errors->has('email') ? ' has-warning' : '' }}">
-                    {{ Form::label('email', trans('auth.email'), [
-                        'class' => 'col-sm-2 col-form-label'
-                    ]) }}
+            <form action="{{ route('auth.login.form') }}" method="POST">
+                <div class="form-group row @error('email') has-warning @enderror">
+                    <label for="email" class="col-sm-2 col-form-label">{{ trans('auth.email') }}</label>
                     <div class="col-sm-10">
-                        {{ Form::email('email', null, [
-                            'class' => 'form-control' . ($errors->has('email') ? ' form-control-warning' : ''),
-                            'placeholder' => trans('pastebin.email_example_placeholder'),
-                            'id' => 'email'
-                        ]) }}
+                        <input type="email" name="email" class="form-control @error('email') form-control-warning @enderror" placeholder="{{ trans('pastebin.email_example_placeholder') }}" id="email" />
 
-                        @if ($errors->has('email'))
+                        @error('email')
                             <div class="form-control-feedback">
-                                {{ $errors->first('email') }}
+                                {{ $message }}
                             </div>
-                        @endif
+                        @enderror
                     </div>
                 </div>
 
-                <div class="form-group row{{ $errors->has('password') ? ' has-warning' : '' }}">
-                    {{ Form::label('password', trans('auth.password'), [
-                        'class' => 'col-sm-2 col-form-label'
-                    ]) }}
+                <div class="form-group row @error('password') has-warning @enderror">
+                    <label for="password" class="col-sm-2 col-form-label">{{ trans('auth.password') }}</label>
                     <div class="col-sm-10">
-                        {{ Form::password('password', [
-                            'class' => 'form-control' . ($errors->has('password') ? ' form-control-warning' : ''),
-                            'placeholder' => '************',
-                            'id' => 'password'
-                        ]) }}
+                        <input type="password" name="password" class="form-control @error('password') form-control-warning @enderror" placeholder="************" id="password" />
 
-                        @if ($errors->has('password'))
+
+                        @error('password')
                             <div class="form-control-feedback">
-                                {{ $errors->first('password') }}
+                                {{ $message }}
                             </div>
-                        @endif
+                        @enderror
                     </div>
                 </div>
 
@@ -50,17 +39,21 @@
                     <label class="col-sm-2"></label>
                     <div class="col-sm-10">
                         <div class="form-check">
-                            {{ Form::customCheckbox('remember_me', trans('auth.remember_me')) }}
+                            <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+
+                            <label class="form-check-label" for="remember">
+                                {{ __('Remember Me') }}
+                            </label>
                         </div>
                     </div>
                 </div>
 
                 <div class="form-group row">
                     <div class="offset-sm-2 col-sm-10">
-                        {{ Form::button('<i class="fa fa-key" aria-hidden="true"></i> ' . trans('auth.login'), [
-                            'class' => 'btn btn-outline-primary',
-                            'type' => 'submit'
-                        ])}}
+                        <button type="submit" class="btn btn-primary">
+                            <i class="fa fa-key" aria-hidden="true"></i> {{ trans('auth.login') }}
+                        </button>
 
                         <a class="btn btn-outline-secondary" href="{{ route('auth.register.form') }}">
                             <i class="fa fa-user-plus" aria-hidden="true"></i> {{ trans('auth.register') }}
@@ -70,7 +63,7 @@
                         </a>
                     </div>
                 </div>
-            {{ Form::close() }}
+            </form>
         </div>
     </div>
 @endsection
