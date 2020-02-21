9ede10daf31091f134014cac9e2802392261d9ba
Macsch15 <macsch15@gmail.com>
2020-02-21 21:01:56 +0100

    Fix autoloader for tests

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -1,6 +1,3 @@
 <?php
-use Application\Autoloader;
 
-require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'Autoloader.php';
-
-new Autoloader();
+require '../vendor/autoload.php';
