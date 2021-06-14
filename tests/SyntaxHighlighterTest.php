4a06b15a71488837a0faf3562372e14db74f8afd
Macsch15 <macsch15@gmail.com>
2021-06-14 19:12:42 +0200

    tests fix kurwo

--- tests/SyntaxHighlighterTest.php
+++ tests/SyntaxHighlighterTest.php
@@ -14,7 +14,7 @@ class SyntaxHighlighterTest extends TestCase
     {
         $parse = preg_replace("/[\t\n\s]+/", '', (new SyntaxHighlighter())->parseCode('Example Text', 'Text'));
 
-        $expected = '<preclass="textpre_code"id="zclip_copy"style="font-family:monospace;"><ol><listyle="font-weight:normal;vertical-align:top;font-size:14px"><divstyle="font:normalnormal1em/1.2emmonospace;margin:0;padding:0;background:none;vertical-align:top;">ExampleText</div></li></ol></pre>';
+        $expected = '<preclass="textpre_code"style="font-family:monospace;"><ol><listyle="font-weight:normal;vertical-align:top;font-size:14px"><divstyle="font:normalnormal1em/1.2emmonospace;margin:0;padding:0;background:none;vertical-align:top;">ExampleText</div></li></ol></pre>';
 
         $this->assertEquals($expected, $parse);
     }
